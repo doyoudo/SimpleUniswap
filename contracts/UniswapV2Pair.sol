696d36989e4863eb3b14a75633673ba2971a2470
doyoudo <1799608748@qq.com>
2024-10-15 12:27:55 +0800

    add some annotation and contract events

--- contracts/UniswapV2Pair.sol
+++ contracts/UniswapV2Pair.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
@@ -17,24 +17,24 @@ contract UniswapV2Pair is ERC20 {
 
     uint public constant MINIMUM_LIQUIDITY = 10**3;
 
+    bool private locked;
+
     constructor() ERC20("UniswapV2 Pair", "UNI-V2") {
         factory = msg.sender;
     }
-
-    function initialize(address _token0, address _token1) external {
-        require(msg.sender == factory, 'UniswapV2: FORBIDDEN');
-        require(_token0 != address(0) && _token1 != address(0), 'UniswapV2: ZERO_ADDRESS');
-        token0 = _token0;
-        token1 = _token1;
-    }
-
-    bool private locked;
 
     modifier lock() {
         require(!locked, "UniswapV2: LOCKED");
         locked = true;
         _;
         locked = false;
+    }
+
+    function initialize(address _token0, address _token1) external {
+        require(msg.sender == factory, "UniswapV2: FORBIDDEN");
+        require(_token0 != address(0) && _token1 != address(0), "UniswapV2: ZERO_ADDRESS");
+        token0 = _token0;
+        token1 = _token1;
     }
 
     function mint(address to) external lock returns (uint liquidity) {
@@ -47,14 +47,15 @@ contract UniswapV2Pair is ERC20 {
         uint _totalSupply = totalSupply();
         if (_totalSupply == 0) {
             liquidity = Math.sqrt(amount0 * amount1) - MINIMUM_LIQUIDITY;
-            _mint(address(this), MINIMUM_LIQUIDITY);
+            _mint(address(this), MINIMUM_LIQUIDITY); // permanently lock the first MINIMUM_LIQUIDITY tokens
         } else {
             liquidity = Math.min(amount0 * _totalSupply / _reserve0, amount1 * _totalSupply / _reserve1);
         }
-        require(liquidity > 0, 'UniswapV2: INSUFFICIENT_LIQUIDITY_MINTED');
+        require(liquidity > 0, "UniswapV2: INSUFFICIENT_LIQUIDITY_MINTED");
         _mint(to, liquidity);
 
         _update(balance0, balance1);
+        emit Mint(msg.sender, amount0, amount1);
     }
 
     function burn(address to) external lock returns (uint amount0, uint amount1) {
@@ -65,7 +66,7 @@ contract UniswapV2Pair is ERC20 {
         uint _totalSupply = totalSupply();
         amount0 = liquidity * balance0 / _totalSupply;
         amount1 = liquidity * balance1 / _totalSupply;
-        require(amount0 > 0 && amount1 > 0, 'UniswapV2: INSUFFICIENT_LIQUIDITY_BURNED');
+        require(amount0 > 0 && amount1 > 0, "UniswapV2: INSUFFICIENT_LIQUIDITY_BURNED");
         _burn(address(this), liquidity);
         _safeTransfer(token0, to, amount0);
         _safeTransfer(token1, to, amount1);
@@ -73,6 +74,7 @@ contract UniswapV2Pair is ERC20 {
         balance1 = IERC20(token1).balanceOf(address(this));
 
         _update(balance0, balance1);
+        emit Burn(msg.sender, amount0, amount1, to);
     }
 
     function swap(uint amount0Out, uint amount1Out, address to) external lock {
@@ -101,6 +103,7 @@ contract UniswapV2Pair is ERC20 {
         }
 
         _update(balance0, balance1);
+        emit Swap(msg.sender, amount0In, amount1In, amount0Out, amount1Out, to);
     }
 
     function getReserves() public view returns (uint112 _reserve0, uint112 _reserve1, uint32 _blockTimestampLast) {
@@ -112,10 +115,17 @@ contract UniswapV2Pair is ERC20 {
     function _update(uint balance0, uint balance1) private {
         reserve0 = uint112(balance0);
         reserve1 = uint112(balance1);
+        emit Sync(uint112(reserve0), uint112(reserve1));
     }
 
+    // This low-level function should be called from a contract which performs important safety checks
     function _safeTransfer(address token, address to, uint value) private {
         (bool success, bytes memory data) = token.call(abi.encodeWithSelector(IERC20.transfer.selector, to, value));
-        require(success && (data.length == 0 || abi.decode(data, (bool))), 'UniswapV2: TRANSFER_FAILED');
+        require(success && (data.length == 0 || abi.decode(data, (bool))), "UniswapV2: TRANSFER_FAILED");
     }
+
+    event Mint(address indexed sender, uint amount0, uint amount1);
+    event Burn(address indexed sender, uint amount0, uint amount1, address indexed to);
+    event Swap(address indexed sender, uint amount0In, uint amount1In, uint amount0Out, uint amount1Out, address indexed to);
+    event Sync(uint112 reserve0, uint112 reserve1);
 }
